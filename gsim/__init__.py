975d3d67ea90e2f6b353a29db7d726be12a16854
zhengjianbing <2448923870@qq.com>
2025-03-04 20:27:25 +0800

    first commit

---
+++ gsim/__init__.py
@@ -0,0 +1,4 @@
+from gsim.experiment_set import AbstractExperimentSet
+from gsim.gfigure import GFigure
+from gsim.experiment_set import OUTPUT_DATA_FOLDER
+from gsim.utils import time_to_str, print_obj_attributes, instr
